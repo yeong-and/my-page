d376184d4afde79ab37686a3fb7f3802ed65be58
yeong-and <yjkang11@gmail.com>
2022-09-20 13:45:56 +0900

    기본화면

--- js/three.js
+++ js/three.js
@@ -2,13 +2,13 @@ import * as THREE from "https://cdn.skypack.dev/three@0.144.0";
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
-  75,
+  40,
   window.innerWidth / window.innerHeight,
   0.1,
   1000
 );
-
 const renderer = new THREE.WebGLRenderer();
+
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
@@ -19,8 +19,8 @@ scene.add(cube);
 
 camera.position.z = 5;
 
-function animate() {
-  requestAnimationFrame(animate);
+function render() {
+  requestAnimationFrame(render);
 
   cube.rotation.x += 0.01;
   cube.rotation.y += 0.01;
@@ -28,4 +28,4 @@ function animate() {
   renderer.render(scene, camera);
 }
 
-animate();
+render();
